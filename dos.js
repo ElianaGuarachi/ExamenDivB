36ecc691a77f78052c9627a9d9895e69dda0bf8c
ElianaGuarachi <eliana.guarachi23@gmail.com>
2021-08-04 11:28:51 -0300

    Parcial resuelto

--- dos.js
+++ dos.js
@@ -1,4 +1,99 @@
 function mostrar()
 {
-    alert("Ejercicio 2");
-}
+   let nacionalidad;
+   let resultado;
+   let edad;
+   let cepa;
+   let porcentajePositivos;
+   let porcentajeNegativos;
+   let contPositivos = 0;
+   let contNegativos = 0;
+   let contResultado = 0;
+   let contAlfa = 0;
+   let contBeta = 0;
+   let contDelta = 0;
+   let cepaAbunda;
+   let flag = 1;
+   let edadContagiado;
+   let argentinosConDelta = 0;
+
+   for(let i = 0; i < 8; i++){
+
+    nacionalidad = prompt("Ingrese nacionalidad: (argentina - extranjera)").toLowerCase();
+    while(nacionalidad != "argentina" && nacionalidad != "extranjera"){
+        nacionalidad = prompt("Error. Ingrese nacionalidad: (argentina - extranjera)").toLowerCase();
+    }
+
+    resultado = prompt("Ingrese resultado (positivo - negativo)").toLowerCase();
+    while(resultado != "positivo" && resultado != "negativo") {
+        resultado = prompt("Error. Reingrese resultado (positivo - negativo)").toLowerCase();
+    }
+
+    edad = parseInt(prompt("Ingrese la edad (18 - 65)"));
+    while(isNaN(edad) || edad < 18 || edad > 65){
+        edad = parseInt(prompt("Error. Reingrese la edad (18-65)"));
+    }
+
+    if(resultado == "negativo") {
+        cepa = prompt("Ingrese el tipo de cepa (delta - alfa - beta - ninguna)");
+        while(cepa != "ninguna") {
+            cepa = prompt("Error. Ingrese el tipo de cepa deberia ser ninguna");
+        }
+    } else {
+        cepa = prompt("Ingrese el tipo de cepa (delta - alfa - beta - ninguna)");
+        while (cepa != "delta" && cepa != "alfa" && cepa != "beta"){
+            cepa = prompt("Error. Ingrese el tipo de cepa (delta - alfa - beta)");
+        }
+    }
+    contResultado ++;
+
+    if(resultado == "positivo"){
+        contPositivos ++;
+    } else {
+        contNegativos ++;
+    }
+
+    if(cepa == "alfa"){
+        contAlfa ++;
+    } else if (cepa == "beta"){
+        contBeta ++;
+    } else {
+        contDelta ++;
+    }
+
+    if(nacionalidad == "extranjera"){
+        if(flag || edad > edadContagiado){
+            edadContagiado = edad;
+            flag = 0;
+        }
+    } else {
+        if (cepa == "delta"){
+            argentinosConDelta ++;
+        }
+    }
+
+   }//fin del for
+
+   porcentajePositivos = contPositivos * 100 / contResultado;
+   porcentajePositivos = porcentajePositivos.toFixed(1);
+   porcentajeNegativos = contNegativos * 100 / contResultado;
+   porcentajeNegativos = porcentajeNegativos.toFixed(1);
+
+   if (contAlfa > contBeta && contAlfa > contDelta){
+       cepaAbunda = "Alfa";
+   } else if (contBeta >= contAlfa && contBeta > contDelta){
+       cepaAbunda = "Beta";
+   } else {
+       cepaAbunda = "Delta";
+   }
+
+
+   document.write("a. Porcentaje de casos positivos: " + porcentajePositivos + "%. <br>");
+   document.write("b. Porcentaje de casos negativos: " + porcentajeNegativos + "%. <br>");
+   document.write("c. La cepa mas encontrada es: " + cepaAbunda + "<br>");
+   document.write("d. Edad del mayor extranjero contagiado: " + edadContagiado + " años. <br>");
+   document.write("e. Cantidad de personas argentina contagiadas con la delta: " + argentinosConDelta + "<br>");
+
+
+
+} // fin de la funcion
--- uno.js
+++ uno.js
@@ -1,5 +1,110 @@
 
 function mostrar()
 {
-	alert("Ejercicio 1");
-}
+	let continuar; 
+	let nombre;
+	let edad;
+	let vacuna;
+	let dosis;
+	let sexo;
+	let promedio;
+	let acumEdadRusa = 0;
+	let contRusa = 0;
+	let nombreMujerMayor;
+	let vacunaMujerMayor;
+	let edadMujerMayor;
+	let flag = 1;
+	let contAmericana = 0;
+	let contAmericanaAdul =0;
+	let porcentaje;
+	let porcentajeDosis;
+	let contVacunados = 0;
+	let contVacunadosDos = 0;
+	let contChina = 0;
+	let vacunaMenosInoculada;
+
+	do {
+		nombre = prompt("Ingrese el nombre de la persona vacunada").toUpperCase();
+		while(nombre.length < 3 || nombre.length > 10) {
+			nombre = prompt("Informacion invalida. Reingrese el nombre de la persona vacunada").toUpperCase();
+		}
+
+		edad = parseInt(prompt("Ingrese la edad: (mayor o igual a 12)"));
+		while(isNaN(edad) || edad < 12){
+			edad = parseInt(prompt("Edad invalida. Reingrese la edad: (mayor o igual a 12)"));
+		}
+		if(edad >= 12 && edad <= 17){
+			vacuna = prompt("Ingrese el tipo de vacuna, entre 12 y 17, solo americana").toLowerCase();
+			while(vacuna != "americana") {
+				vacuna = prompt("Informacion invalida. Ingrese el tipo de vacuna, entre 12 y 17, solo americana").toLowerCase();
+
+			}
+		} else {
+			vacuna = prompt("Ingrese el tipo de vacuna administrada (rusa, china, americana)").toLowerCase();
+			while(vacuna != "rusa" && vacuna != "china" && vacuna != "americana"){
+				vacuna = prompt("Error. Reingrese el tipo de vacuna administrada (rusa, china, americana)").toLowerCase();
+			}
+		}
+
+		dosis = prompt("Ingrese “p” en caso de ser la primer dosis o “s” en caso de ser la segunda dosis").toLowerCase();
+		while(dosis != 'p' && dosis != 's'){
+			dosis = prompt("Error. Ingrese “p” en caso de ser la primer dosis o “s” en caso de ser la segunda dosis").toLowerCase();
+		}
+
+		sexo = prompt("Ingrese el sexo (f - m)").toLowerCase();
+		while(sexo != 'f' && sexo != 'm') {
+			sexo = prompt("Error. Ingrese el sexo (f - m)").toLowerCase();
+		}
+
+		if(vacuna == "rusa"){
+			acumEdadRusa += edad;
+			contRusa++;
+		} else if (vacuna == "americana") {
+			contAmericana ++;
+			if (edad > 17){
+				contAmericanaAdul ++;
+			}
+		} else {
+			contChina++;
+		}
+
+		if(sexo == "f"){
+			if (flag || edad > edadMujerMayor) {
+				edadMujerMayor = edad;
+				nombreMujerMayor = nombre;
+				vacunaMujerMayor = vacuna;
+				flag = 0;
+			}
+		}
+		
+		contVacunados ++;
+		if(dosis == "s"){
+			contVacunadosDos ++;
+		}
+
+		continuar = prompt("Quiere continuar ingresando datos? (si / no)");
+	} while (continuar == "si");
+
+	promedio = acumEdadRusa / contRusa; //a
+
+	porcentaje = contAmericanaAdul * 100 / contAmericana; //c
+	porcentaje = porcentaje.toFixed(1);
+
+	porcentajeDosis = contVacunadosDos * 100 / contVacunados; //d
+	porcentajeDosis = porcentajeDosis.toFixed(1);
+
+	if(contChina < contRusa && contChina < contAmericana){
+		vacunaMenosInoculada = "China";
+	} else if(contRusa <= contChina && contRusa < contAmericana){
+		vacunaMenosInoculada = "Rusa";
+	} else {
+		vacunaMenosInoculada = "Americana";
+	}
+
+	document.write("a. El promedio de edad de los que se vacunaron con la vacuna rusa: " + promedio + "<br>");
+	document.write("b. Nombre de la mujer con mas edad: " + nombreMujerMayor + ", con la vacuna: " + vacunaMujerMayor + "<br>");
+	document.write("c. De las personas que recibieron la vacuna americana, el " + porcentaje + "% es mayor de edad. <br>")
+	document.write("d. Porcentaje de los que estan vacunados con 2 dosis: " + porcentajeDosis + "% del total de vacunados. <br>");
+	document.write("e. La vacuna menos inoculada es: " + vacunaMenosInoculada);
+
+} // fin de la funcion
